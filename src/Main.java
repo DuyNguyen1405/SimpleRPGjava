249ec12f43b77e3015b2c4f4fd90d55969222c8f
Kien Do <kiendo95@gmail.com>
2016-03-07 21:24:14 +0700

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by j on 07/03/2016.
+ */
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Main {
+   public static void main(String[] argv){
+       JFrame myFrame = new JFrame("Sample Frame");
+       myFrame.setSize(300,400);
+       myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       myFrame.setVisible(true);
+   }
+}

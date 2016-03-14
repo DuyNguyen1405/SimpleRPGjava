a51a210150ff0213471f3f4dedd35c564cf43f6c
Duy Nguyen <duyso1000@gmail.com>
2016-03-14 21:46:05 +0700

    test

--- src/layout/Controller.java
+++ src/layout/Controller.java
@@ -1,5 +1,29 @@
 package layout;
 
 public class Controller {
-
+	private Layout layout;
+	private static void createAndShowGUI()  {
+		 
+        JFrame frame1 = new JFrame("JAVA");
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JButton button = new JButton(" Test");
+        //Add action listener to button
+        button.addActionListener(new ActionListener() {
+ 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+        });      
+ 
+        frame1.getContentPane().add(button);
+        frame1.pack();
+        frame1.setVisible(true);
+    }
+	
+    public static void main(String[] args) {
+    	createAndShowGUI();
+    }
 }

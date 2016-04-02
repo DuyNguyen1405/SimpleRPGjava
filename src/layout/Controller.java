43deab3fdee54075cf1e3b433345da6905ac6747
Kien Do <kiendo95@gmail.com>
2016-04-02 22:35:17 +0700

    checkout

--- src/layout/Controller.java
+++ src/layout/Controller.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package layout;
 
 import java.awt.event.ActionEvent;
@@ -11,39 +11,39 @@ import character.player;
 public class Controller {
 	Object nextValue = new Object();
 
-	
-	public void press(JButton button_up, JButton button_down, JButton button_left, JButton button_right, final JTable table)  {
-    	
-       	final Layout layout = new Layout();
-       	player player = new player();
 
-    	button_up.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e)  {
-               	int x = layout.getX();
-               	int y = layout.getY();
+	public void press(JButton button_up, JButton button_down, JButton button_left, JButton button_right, final JTable table) {
 
-				if (x < 1){
+		final Layout layout = new Layout();
+		player player = new player();
+
+		button_up.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int x = layout.getX();
+				int y = layout.getY();
+
+				if (x < 1) {
 					System.out.println("Khong the di chuyen");
 					return;
 				}
-            	
-               	//player.makeChar(x-1, y, table);
-               	Object next = table.getValueAt(x-1, y);
-               	table.setValueAt("o", x-1, y);
-            	layout.setX(x-1);
-            	table.setValueAt(layout.getOldPos(), x, y);
-            	layout.setOldPos(next);
-            	
-            }
-        });
+
+				//player.makeChar(x-1, y, table);
+				Object next = table.getValueAt(x - 1, y);
+				table.setValueAt("o", x - 1, y);
+				layout.setX(x - 1);
+				table.setValueAt(layout.getOldPos(), x, y);
+				layout.setOldPos(next);
+
+			}
+		});
 
 		button_down.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)  {
+			public void actionPerformed(ActionEvent e) {
 				int x = layout.getX();
 				int y = layout.getY();
 
 				System.out.println(x);
-				if (x > 3){
+				if (x > 3) {
 					System.out.println("Khong the di chuyen");
 					return;
 				}
@@ -57,7 +57,8 @@ public class Controller {
 			}
 		});
 	}
-	
+
+}
 //	public void press(JButton button,JTable table, Object oldPos)  {
 //    	
 //       	Layout layout = new Layout();
@@ -75,77 +76,3 @@ public class Controller {
 //            }
 //        });  
 //    }
- 
- 
-}
-=======
-package layout;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTable;
-
-import character.player;
-public class Controller {
-	Object nextValue = new Object();
-
-	
-	public void press(JButton button_up, JButton button_down, JButton button_left, JButton button_right, final JTable table)  {
-    	
-       	final Layout layout = new Layout();
-       	player player = new player();
-
-    	button_up.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e)  {
-               	int x = layout.getX();
-               	int y = layout.getY();
-            	
-               	//player.makeChar(x-1, y, table);
-               	Object next = table.getValueAt(x-1, y);
-               	table.setValueAt("o", x-1, y);
-            	layout.setX(x-1);
-            	table.setValueAt(layout.getOldPos(), x, y);
-            	layout.setOldPos(next);
-            	
-            }
-        });
-
-		button_down.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e)  {
-				int x = layout.getX();
-				int y = layout.getY();
-
-				//player.makeChar(x-1, y, table);
-				Object next = table.getValueAt(x + 1, y);
-				table.setValueAt("o", x + 1, y);
-				layout.setX(x + 1);
-				table.setValueAt(layout.getOldPos(), x, y);
-				layout.setOldPos(next);
-
-			}
-		});
-	}
-	
-//	public void press(JButton button,JTable table, Object oldPos)  {
-//    	
-//       	Layout layout = new Layout();
-//       	player player = new player();
-//
-//    	button.addActionListener(new ActionListener() {
-//            public void actionPerformed(ActionEvent e)  {
-//               	int x = layout.getX();
-//               	int y = layout.getY();
-//            	player.makeChar(x-1, y, table);
-//               	table.setValueAt("o", x-1, y);
-//            	layout.setX(x-1);
-//            	table.setValueAt(player.getOldValue(), x, y);
-//            	
-//            }
-//        });  
-//    }
- 
- 
-}
->>>>>>> remotes/origin/develop
